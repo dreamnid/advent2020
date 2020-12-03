b6f6511bfb88cebbe4623370369cd81fef5c60fa
Giovanni Go <dreamnid@dreamnid.com>
2020-12-02 23:03:38 -0800

    Add solution for puzzle 3

---
+++ 03/3a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import itertools
+
+INPUT_FILE = '3-input.txt'
+#INPUT_FILE = 'example-input.txt'
+
+rows = []
+with open(INPUT_FILE) as fh:
+    for line in fh:
+        rows.append(line.strip())
+
+col_idx = 0
+trees = 0
+
+# Move: Down 1 row, right 3
+for row in rows:
+    # for j in range(len(row)):
+    #     if j == col_idx:
+    #         if row[j] == '.':
+    #             print('O', end='')
+    #         else:
+    #             print('X', end='')
+    #     else:
+    #         print(row[j], end='')
+    # print('')
+
+    if row[col_idx] == '#':
+        trees += 1
+    col_idx += 3
+    col_idx %= len(row) 
+
+print('Trees', trees)
+    
---
+++ 03/3b.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+from functools import partial, reduce
+
+INPUT_FILE = '3-input.txt'
+#INPUT_FILE = 'example-input.txt'
+
+rows = []
+with open(INPUT_FILE) as fh:
+    for line in fh:
+        rows.append(line.strip())
+
+def find_trees(rows, right, down=1) -> int:
+    trees = 0
+
+    row_idx = 0
+    col_idx = 0
+    for row_idx in range(0, len(rows), down):
+        row = rows[row_idx]
+        if False:
+            for j in range(len(row)):
+                if j == col_idx:
+                    if row[j] == '.':
+                        print('O', end='')
+                    else:
+                        print('X', end='')
+                else:
+                    print(row[j], end='')
+            print('')
+
+        if row[col_idx] == '#':
+            trees += 1
+        col_idx += right
+        col_idx %= len(row) 
+
+    return trees
+
+find_trees_rows = partial(find_trees, rows)
+
+desired = [
+    find_trees_rows(1),
+    find_trees_rows(3,),
+    find_trees_rows(5,),
+    find_trees_rows(7,),
+    find_trees_rows(1,2),
+]
+
+res = reduce(lambda a, b: a*b, desired)
+print(f'Res {res}')
+    
