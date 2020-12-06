a671fe2cd96f49fb17ff4c9da6f96c70381355ab
Giovanni Go <dreamnid@dreamnid.com>
2020-12-06 12:47:34 -0800

    Rewrite to leverage get_file_contents

--- 06/6b.py
+++ 06/6b.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python3
-import math
-
-from functools import reduce
 from typing import List, Set
 
-INPUT_FILE='6-input.txt'
-INPUT_FILE='6-example.txt'
+# Fix path so we can do a relative import: https://stackoverflow.com/a/27876800
+if __name__ == '__main__':
+    if __package__ is None:
+        import sys
+        from os import path
+        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
+        # Relative imports here
+        from util import *
+
+INPUT_FILE='6-input.txt'
+#INPUT_FILE='6-example.txt'
 
 def any_agree(answers: List[Set[str]]) -> bool:
     # Return question(s) that received at least 1 yes from anyone in the group
+#    print(answers)
     return set.union(*answers)
 
 def all_must_agree(answers: List[Set[str]]) -> bool:
     # Return question(s) that received yes from everyone in the group
     return set.intersection(*answers)
 
-def get_answers() -> List[Set[str]]:
-    # Note that this depends on the file having an extra blank line at the end
+# For each group, collect answers a list of set where each set contains the answer from each person
+# e.g. [{a, b, c}, {a}]
+forms = list(map(lambda x: [set(i) for i in x], get_file_contents(INPUT_FILE)))
 
-    with open(INPUT_FILE) as fh:
-        tmp = []
-        for line in fh:
-            line = line.strip()
-
-            if line != '':
-                line_chars = {char for char in line}
-                tmp.append(line_chars)
-            else:
-                yield tmp
-                tmp=[]
-
-
-
-any_res = [len(any_agree(answers)) for answers in get_answers()]
+#print(forms)
+any_res = [len(any_agree(form)) for form in forms]
 print(f'Any yes Sum: {sum(any_res)}')
-all_res = [len(all_must_agree(answers)) for answers in get_answers()]
+all_res = [len(all_must_agree(form)) for form in forms]
 print(f'All agreed Sum: {sum(all_res)}')
