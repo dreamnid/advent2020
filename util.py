8422f75b451c5ede6d5dda0c047f9684892ce458
Giovanni Go <dreamnid@dreamnid.com>
2020-12-06 12:47:23 -0800

    Add new util

---
+++ util.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def get_file_contents(file: str) -> List[List[str]]:
+    """
+    Process the input file
+
+    Some of AoC puzzles formats the input file to use a blank line to designate a group.
+    :param file: the name of the file
+    :return:
+    """
+    with open(file) as fh:
+        content = fh.read()
+        return [block.split('\n') for block in content.split('\n\n')]
