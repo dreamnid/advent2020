d2f35b8b2c38d65fd2be33f064e4eae26237fd0f
Giovanni Go <dreamnid@dreamnid.com>
2020-12-08 21:53:27 -0800

    Move to util

--- 01/1b.py
+++ 01/1b.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 from functools import reduce
-from typing import List, Union
+
+
+# Fix path so we can do a relative import: https://stackoverflow.com/a/27876800
+if __name__ == '__main__':
+    if __package__ is None:
+        import sys
+        from os import path
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+        # Relative imports here
+        from util import *
 
 INPUT_FILE = '1-input.txt'
 
@@ -36,51 +46,7 @@ def find_3_parts_sum(input, desired_sum):
     # Didn't find a result
     return None
 
-def find_var_parts_sum(input_list: List[int], desired_sum: int, num_addends=3, addends=None, addend_idx=0) -> Union[List[int], None]:
-    """
-    Find the combination of numbers in the input_list that adds up to desired_sum
 
-    :param input_list: The *sorted* list of available numbers
-    :param desired_sum: The target sum that the addends should add to
-    :param num_addends: The desired number of addends that should add up to desired_sum. [e.g. 2 mean a+b, 3 means a+b+c, etc.]
-    :param addends: private - buffer holding current result
-    :param addend_idx: private - index of the addend list that is currently being worked on
-    :return: The list of numbers that add up to the desired_sum or None if no solution found
-
-    Note that this is a recursive call. Each recursion call works on the next addend hence why we pass
-    the input_list without the first element and 1 is added to addend_idx
-    """
-    if not addends:
-        # Using None as initial value to help assert if I'm doing something dumb later
-        addends = [None] * num_addends
-
-    if addend_idx == num_addends-1:
-        # If we're looking at the last addend, we can calculate the last number needed instead of iterating through
-        # the input_list
-        input_set = set(input_list)
-        cur_sum = sum(addends[:-1])
-        desired_addend = desired_sum - cur_sum
-        if desired_addend in input_set:
-            addends[addend_idx] = desired_addend
-            return addends
-
-        return None
-
-    for input in input_list:
-        addends[addend_idx] = input
-
-        if sum(addends[:addend_idx]) > desired_sum:
-            # sum is too large, so don't need to recurse again since the rest of the list is bigger
-            return None
-        temp_res = find_var_parts_sum(input_list[1:], desired_sum, num_addends=num_addends, addends=addends, addend_idx=addend_idx+1)
-        if temp_res:
-            return temp_res
-
-    if None not in addends and sum(addends) == desired_sum:
-        return addends
-
-    # Didn't find a result
-    return None
 
 #result = find_3_parts_sum(input, DESIRED_SUM)
 result = find_var_parts_sum(input, DESIRED_SUM, 3)
