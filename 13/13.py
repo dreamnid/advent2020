8ded83c7f5875a61b994c08ac907c47533a40bc8
Giovanni Go <dreamnid@dreamnid.com>
2020-12-13 07:43:31 -0800

    Add brute force solution

--- 13/13.py
+++ 13/13.py
@@ -7,6 +7,8 @@ import re
 from time import time
 from typing import Dict, List, Set, Union
 
+import humanize
+
 # Fix path so we can do a relative import: https://stackoverflow.com/a/27876800
 if __name__ == '__main__':
     if __package__ is None:
@@ -18,7 +20,9 @@ if __name__ == '__main__':
         from util import *
 
 INPUT_FILE='13-input.txt'
-INPUT_FILE='13a-example.txt'
+#INPUT_FILE='13a-example.txt'
+#INPUT_FILE='13b-example.txt'
+#INPUT_FILE='13c-example.txt'
 
 inputs = [line for line in get_file_contents(INPUT_FILE)[0]]
 desired_time = int(inputs[0])
@@ -35,8 +39,10 @@ def a():
 
 def b():
     input_split = inputs[1].split(',')
-    print(input_split)
+    print(len(input_split), input_split)
     bus_schedule = {i:int(x) for i, x in enumerate(input_split) if x != 'x'}
+    print(bus_schedule)
+
 
     any_simulatenous_buses = []
     for offset, bus_itr in bus_schedule.items():
@@ -44,30 +50,77 @@ def b():
             any_simulatenous_buses.append((bus_itr+offset, bus_schedule[bus_itr+offset]))
 
     print(any_simulatenous_buses)
-    time = 1068788
-    found = True
-    cur_time = 0
+    try:
+        increment = any_simulatenous_buses[0][0] * any_simulatenous_buses[0][1]
+        diff=any_simulatenous_buses[0][0]
+    except:
+        increment = bus_schedule[0]
+        diff = 0
+    print(increment)
+
+    time = 100000000000000
+    #time = 0
     while True:
-        print(time)
-        cur_time = time -7
-        for offset, bus_itr in bus_schedule.items():
-            found &= ((cur_time + offset) % bus_itr) == 0
-            print('itr: ', offset, cur_time+offset, bus_itr, found)
-            if not found:
-                break
-
-        if found:
+        if time % increment == 0:
             break
+        time += 1
+    print('starting at ', time)
 
-        time += 133
+    cur_time = finder(time, increment, diff, bus_schedule)
 
-        if time > 1068789:
-            break
-
-    if found:
-        print(cur_time)
+    if cur_time:
+        print('answer:', cur_time)
     else:
         print('nope')
 
-a()
+def finder(start_time, increment, subtract, bus_schedule):
+    time = start_time
+    cur_time = 0
+    try:
+        while True:
+            if cur_time % 1000 == 0 and False:
+                print(humanize.intcomma(cur_time))
+            #print(time)
+            found = True
+            cur_time = time - subtract
+            for offset, bus_itr in bus_schedule.items():
+                found &= ((cur_time + offset) % bus_itr) == 0
+                #print('itr: ', offset, cur_time+offset, bus_itr, found)
+                if not found:
+                    break
+
+            if found:
+                break
+
+            time += increment
+    except KeyboardInterrupt:
+        print('Interrupted, time at ', time)
+        return None
+
+    return cur_time
+#a()
 b()
+
+def b_debug():
+    input_split = inputs[1].split(',')
+    print(len(input_split), input_split)
+    bus_schedule = {i:int(x) for i, x in enumerate(input_split) if x != 'x'}
+    start = 3410
+    limit = start + 50
+
+    print('    ', end='')
+    for x in range(start, limit):
+        print('{:02d}'.format(x), end=' ')
+
+    print()
+
+    for offset, bus_itr in bus_schedule.items():
+        print('{:02d}  '.format(bus_itr), end='')
+        for x in range(start, limit):
+            if (x-offset) % bus_itr == 0:
+                print('D ', end=' ')
+            else:
+                print('  ', end=' ')
+        print()
+
+#b_debug()
