b9e7ded014177c4510b31b739c0fe26a84a9a026
Giovanni Go <dreamnid@dreamnid.com>
2020-12-15 07:08:50 -0800

    Done as generator for the funsies

--- 15/15.py
+++ 15/15.py
@@ -6,7 +6,7 @@ from operator import mul
 import os
 import re
 from time import time
-from typing import Dict, List, Set, Tuple, Union
+from typing import Dict, Generator, List, Set, Tuple, Union
 
 from humanize import intcomma
 
@@ -21,7 +21,7 @@ if __name__ == '__main__':
         from util import *
 
 INPUT_FILE='15-input.txt'
-#INPUT_FILE='15a-example.txt'
+INPUT_FILE='15a-example.txt'
 
 input = [int(num) for num in get_file_contents(INPUT_FILE)[0][0].split(',')]
 
@@ -39,11 +39,45 @@ def nth_num_spoken(num:int) -> int:
         new = cur_num
     return cur_num
 
+def gnome_talker(limit: int) -> Generator[int, int, None]:
+    """
+    Generates the nth word spoken
+
+    Note that the generator accepts send to set a new limit that must be higher than the previous limits.
+    Done this way to learn how send works. In reality, would yield each number that was spoken
+
+    :param limit: The nth word desired - e.g. 10 for the 10th word spoken
+    """
+    seen = {num:idx for idx, num in enumerate(input[0:-1])}
+    new = input[-1]
+    cur_num = input[-1]
+    i = len(input)
+    while True:
+        if cur_num in seen and seen[cur_num] != (i-1):
+            cur_num = i - seen[cur_num] - 1
+        else:
+            cur_num = 0
+
+        seen[new] = i - 1
+        new = cur_num
+
+        if i == limit-1:
+            send = yield cur_num
+            if send is not None and send > i:
+                limit = send
+            else:
+                break
+        i += 1
+
+
 start = time()
-print('2020th number spoken:', nth_num_spoken(2020))
+talker = gnome_talker(2020)
+print('2020th number spoken:', next(talker))
 startb = time()
 print('a timing: ', startb - start)
-print('30000000th number spoken:', nth_num_spoken(30000000))
+
+# Use send to set new goal
+print('30000000th number spoken:', talker.send(3000))
 print('b timing: ', time() - startb)
 print()
 print('overall timing:', time() - start)
