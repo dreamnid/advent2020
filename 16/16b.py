89b48e4e9a459f99c2a26795d34b740a7040222d
Giovanni Go <dreamnid@dreamnid.com>
2020-12-16 01:14:34 -0800

    Add solution for 16b

--- 16/16b.py
+++ 16/16b.py
@@ -37,16 +37,13 @@ for cur_line in rules_input:
     ranges = [int(ranges_match.group(i)) for i in range(1, 5)]
     rules[type] = ranges
 
-print(rules)
-
-def part_a()
-
-ordering = [None]  * len(rules)
 field_idx: Dict[str, int] = {}
 not_valid = []
 valid_tickets = []
 not_valid_rule = defaultdict(set)
 valid_rule = defaultdict(set)
+
+# Get rid of bad tickets
 for cur_ticket in others:
     ticket_valid = True
     for pos, number in enumerate(cur_ticket):
@@ -64,74 +61,90 @@ for cur_ticket in others:
     if ticket_valid:
         valid_tickets.append(cur_ticket)
 
-print('hi', mine + valid_tickets)
+# Group all the field values together. The ith element in the list contains the ith value in all the tickets
+fields_values = [sorted(list(i)) for i in zip(*(mine+valid_tickets))]
 
-for cur_ticket in mine + valid_tickets:
-    for pos, number in enumerate(cur_ticket):
-        valid = False
+print('pos | values for that position')
+for i, row in enumerate(fields_values):
+    print(f'{i:03d} | ', end=' ')
+    for col in row:
+        print(f'{col:03d}', end=' ')
+    print()
 
-        for rule, constraints in rules.items():
-            valid |= (constraints[0] <= number <= constraints[1]) or (constraints[2] <= number <= constraints[3])
+pos_to_rule_poss = [set() for i in range(len(rules))]
+count = 0
+
+for rule, constraints in rules.items():
+    for pos, cur_field_values in enumerate(fields_values):
+        valid = True
+        for number in cur_field_values:
+            valid &= (constraints[0] <= number <= constraints[1]) or (constraints[2] <= number <= constraints[3])
             #print(number, valid)
 
-            if valid and pos not in not_valid_rule[rule]:
-                valid_rule[rule].add(pos)
-            else:
-                if pos in valid_rule[rule]:
-                    valid_rule[rule].remove(pos)
-                not_valid_rule[rule].add(pos)
-
-        if not valid:
-            break
-
-print(dict(valid_rule))
-poss_rule_combos = []
-
-from itertools import product
-for i in product(*list(valid_rule.values())):
-    valid = True
-    seen = set()
-    for j in i:
-        if j not in seen:
-            seen.add(j)
-        else:
-            valid = False
-            break
-    if valid:
-        poss_rule_combos.append(i)
-print(poss_rule_combos)
-
-
-rules_name_idx = list(valid_rule.keys())
-rules_list = [rules[rule] for rule in valid_rule.keys()]
-print('ruleslist', rules_list)
-
-
-not_valid_rule = defaultdict(set)
-valid_rule = defaultdict(set)
-good_combo = None
-for cur_field_combo in poss_rule_combos:
-    valid = True
-    for cur_ticket in mine + valid_tickets:
-        valid = True
-        for cur_pos in cur_field_combo:
-            number = cur_ticket[cur_pos]
-            constraints = rules_list[cur_pos]
-
-            valid &= (constraints[0] <= number <= constraints[1]) or (constraints[2] <= number <= constraints[3])
-
             if not valid:
+                if pos == 0 and False:
+                    count += 1
+                    print('Not valid due to ', number, rule, constraints, pos, count)
                 break
-    if valid:
-        good_combo = cur_field_combo
-        break
-assert good_combo
-print(good_combo)
 
-answer_b = []
-for i, field in enumerate(mine):
-    if rules_name_idx[good_combo[i]].startswith('departure'):
-        answer_b.append(field)
+        if valid:
+            if pos == 0 and False:
+                print('Adding', rule, constraints, pos, count)
+            valid_rule[rule].add(pos)
+            pos_to_rule_poss[pos].add(rule)
 
-print('part a:', sum(not_valid))
-print('part b:', reduce(mul, answer_b, 1))
+
+print('Poss position per rule:', dict(valid_rule))
+from collections import Counter
+
+print('number of poss rules per position:', Counter([j for i in valid_rule.values() for j in i]))
+
+print()
+print('Possible rules per position')
+for i, pos in enumerate(pos_to_rule_poss):
+    print(f'{i:03d} | ', ', '.join(sorted(list(pos))))
+
+def finder(assigned=None, used=None):
+    if assigned is None:
+        assigned = [None] * len(rules)
+    if used is None:
+        used = set()
+
+    if sum([rule is None for rule in assigned]) == 0:
+        return assigned
+    while True:
+        # Find position with smallest set
+        value_to_beat = [None]*len(rules)
+        least_pos = None
+        for i, pos in enumerate(pos_to_rule_poss):
+            if assigned[i]:
+                continue
+            modified_pos = pos - used
+            if len(modified_pos) == 0:
+                return
+            elif len(modified_pos) < len(value_to_beat) and assigned[i] is None:
+                value_to_beat = modified_pos
+                least_pos = i
+
+        if len(value_to_beat) == 1:
+            assigned[least_pos] = value_to_beat.pop()
+            used.add(assigned[least_pos])
+        else:
+            for poss_rule2 in value_to_beat:
+                assigned2 = assigned.copy()
+                assigned2[least_pos] = poss_rule2
+                used2 = used.copy()
+                used2.add(poss_rule2)
+                res = finder(assigned2, used2)
+                if res:
+                    return res
+
+        if sum([rule is None for rule in assigned]) == 0:
+            return assigned
+
+print()
+assigned = finder()
+mine = mine[0]
+answer_b = [field_value for i, field_value in enumerate(mine) if assigned[i].startswith('departure')]
+
+print('part b:', answer_b, reduce(mul, answer_b, 1))
